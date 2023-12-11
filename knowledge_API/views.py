b6a37de6cd371ed68f83f17a745971ef7e08ddb9
rockroman <rock.rakic@gmail.com>
2023-12-11 00:49:32 +0000

    fix:dj rest auth logout view

--- knowledge_API/views.py
+++ knowledge_API/views.py
@@ -3,6 +3,10 @@
 # 3rd party:
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .settings import (
+    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
+    JWT_AUTH_SECURE,
+)
 
 
 # Internal:
@@ -15,3 +19,26 @@ def root_route(request):
     return Response({
         "message": "Welcome to my Knowledge API!"
     })
+
+@api_view(['POST'])
+def logout_route(request):
+    response = Response()
+    response.set_cookie(
+        key=JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    response.set_cookie(
+        key=JWT_AUTH_REFRESH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    return response
